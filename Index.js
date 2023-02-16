ec330d0fde6aa539abe097bb21ed550db861a654
Ankit <ankitpal3106@gmail.com>
2023-02-16 14:34:55 +0530

    Initial commit

---
+++ Index.js
@@ -0,0 +1,9 @@
+
+
+// let typingEffect = new Typed('.multiText', {
+//     String : ['Coder', 'Front-end Developer', 'Designer'],
+//     loop : true,
+//     typeSpeed : 100,
+//     backSpeed : 80,
+//     backDelay : 1500
+// });
